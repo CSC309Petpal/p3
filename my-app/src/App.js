0327e753836f8cd10773fb1b32192acae03717a0
YuouLei <leoyuoulei@gmail.com>
2023-12-05 14:48:19 -0500

    login

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,21 @@
-import logo from './logo.svg';
+
+import React from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Header from './components/Header/header'
-
+import Header from './components/Header/header';
+import Login from './pages/Login/LoginPage';
 function App() {
   return (
-    <Header></Header>
+    <BrowserRouter>
+      
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        {/* Add more routes as needed */}
+        {/* Optional: Default route */}
+        <Route path="/" element={<Navigate to="/login" />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- my-app/src/pages/Login/LoginPage.jsx
+++ my-app/src/pages/Login/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useContext, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { BACKENDHOST } from "./config";
+//import { BACKENDHOST } from "./config";
 
 
 import LoginInput from "../../components/input/LoginInput";
