790176280d022a5630e176f76c17e818f27c4bc7
MacroSony <jameszh.wang@mail.utoronto.ca>
2023-12-07 17:11:25 -0500

    change

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -22,13 +22,15 @@ import ShelterComponent from './pages/ShelterManage';
 import ShelterUpdateForm from './pages/ShelterUpdate';
 import SeekerUpdateForm from './pages/SeekerUpdate';
 
-import NotificationBoard from './components/Notification/notificationBoard';
+// import NotificationBoard from './components/Notification/notificationBoard';
+import NotificationPage from './pages/Notification/NotificationPage';
 import PetListing from './pages/PetListing';
 import PetUpdateForm from './pages/PetUpdate';
 import SeekerDetail from './pages/SeekerDetail';
 
 
 import Landing from './pages/Landing/landing';
+
 
 
 function App() {
@@ -83,7 +85,7 @@ function App() {
         <Route path="/pet/update/:petId" element={<PetUpdateForm />} />
 
 
-        <Route path="/notifications" element={< NotificationBoard/>} />
+        <Route path="/notifications" element={< NotificationPage/>} />
         <Route path="/*" element={<NotFound/>}/>
         {/* Add more routes as needed */}
         {/* Optional: Default route */}
