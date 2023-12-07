d0e13e8e65956aebe263feaf80f189f77e525a11
YuouLei <leoyuoulei@gmail.com>
2023-12-07 17:45:52 -0500

    application creation

--- my-app/src/pages/Applications/ApplicationCreationForm.jsx
+++ my-app/src/pages/Applications/ApplicationCreationForm.jsx
@@ -1,49 +1,45 @@
+import React, { useState } from 'react';
 
-function ApplicationCreationForm(props){
+function ApplicationCreationForm(props) {
+  // Rename 'experience' to 'details' to match the textarea id
+  const [formData, setFormData] = useState({
+    details: ''
+  });
+
+  const handleChange = (event) => {
+    const { id, value } = event.target;
+    setFormData(prevFormData => ({
+      ...prevFormData,
+      [id]: value
+    }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    props.onSubmit(formData); // Pass formData on submit
+  };
+
   return (
-  
-  <div className="container mt-5 mb-auto">
-      
-  <div className="row mb-2">
-      {/* Application Section*/}
-      <div className="col-md-4">
-          <div className="col mb-5">
-              <span className="h3"> Adopting for</span>
-          </div>
+    <div className="container mt-5 mb-auto">
+      <form onSubmit={handleSubmit}>
+        {/* Details textarea */}
+        <div className="mb-4">
+          <label htmlFor="details" className="form-label">Prior Experience</label>
+          <textarea
+            className="form-control"
+            id="details"
+            rows="3"
+            placeholder="Describe your prior experience with pets"
+            value={formData.details} // Update to use formData.details
+            onChange={handleChange}
+            required
+          />
+        </div>
 
-          <div className="mb-3">
-       
-              <img /*src={img} REPLACE WHEN FINISHED */ alt="Pet you are applying right now" className="img-fluid mx-auto d-block "/>
-          </div>
-      
-      </div>
-      
-      {/*<!-- Form Section -->*/}
-      <div className="col-md-7 ms-auto">
-          <div className = "col md-auto mb-3">
-              <span className="h3"> Applicant's information: </span>
-          </div>
+        <button type="submit" className="btn btn-light" id="button">Submit</button>
+      </form>
+    </div>
+  );
+}
 
-          <form id ="adoption_form" onSubmit={props.function}>
-              <div className="mb-3">
-                  <label for="userName" className="form-label">Name</label>
-                  <input type="text" className="form-control" id="userName" placeholder="Enter your name" required/>
-              </div>
-              <div className="mb-3">
-                  <label for="userEmail" className="form-label">Email</label>
-                  <input type="email" className="form-control" id="userEmail" placeholder="Enter your email" required/>
-              </div>
-              <div className="mb-4">
-                  <label for="experience" className="form-label">Prior Experience</label>
-                  <textarea className="form-control" id="experience" rows="3" placeholder="Describe your prior experience with pets" required></textarea>
-              </div>
-             
-              <button type="submit" className="btn btn-light" id = "button" >Submit</button>
-
-  
-          </form>
-      </div>
-  </div>
-</div>
-  )};
 export default ApplicationCreationForm;
